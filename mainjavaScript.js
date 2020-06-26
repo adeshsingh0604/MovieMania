0c38d828a5cfcce1183cfca6031e3b5778d03f77
adeshsingh0604 <adeshsingh0604@users.noreply.github.com>
2020-06-26 16:08:28 +0530

    second commit

--- mainjavaScript.js
+++ mainjavaScript.js
@@ -1,3 +1,4 @@
+// delete element
 var list = document.querySelector('.bgs ul');
 
 list.addEventListener('click',function(e){
@@ -6,8 +7,9 @@ list.addEventListener('click',function(e){
         list.removeChild(li);
 
     }
-})
+});
 
+// hide element
 var hide = document.getElementById('hide1');
 hide.addEventListener('click',function(e){
     if(hide.checked == true){
@@ -16,3 +18,34 @@ hide.addEventListener('click',function(e){
         list.hidden= false;
     }
 });
+
+// add element
+
+var form = document.forms['add'];
+form.addEventListener('sumbit',function(e){
+    
+    e.preventDefault(); 
+    
+    var val1=form.querySelector('input[type="text"]').value;
+    console.log(val1)
+    
+    var li=document.createElement('li');
+    
+    var label=document.createElement('label');
+    
+    var buttonspan=document.createElement('button');
+    
+    li.appendChild(label);
+    li.appendChild(buttonspan);
+    list.appendChild(li);
+
+    label.textContent = val1;
+    buttonspan.textContent = 'label';
+    
+    label.classList.add('name');
+    buttonspan.classList.add('Submitspan');
+
+});
+
+
+
